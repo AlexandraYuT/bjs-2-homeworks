e4de26e29fc8fee5d2c1276da77e096502ce5d3d
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-22 21:24:46 +0400

    task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,48 @@
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+// Задание 1
+function getArrayParams(arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	avg = parseFloat((sum / arr.length).toFixed(2));
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+	}
+	return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+	let max = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let arrSum = func(arrOfArr[i]);
+		if (arrSum > max) {
+			max = arrSum;
+		}
+	}
+	return max;
 }
 
-function differenceEvenOddWorker(...arr) {
 
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+	return getArrayParams(arr).max - getArrayParams(arr).min;
 }
