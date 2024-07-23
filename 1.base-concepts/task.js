8f15c0f735b26e0d65ce354faca61cdf9b258a6f
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-23 23:58:54 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,18 @@
-"use strict";
+"use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = b ** 2 - 4 * a * c;
-	if (d > 0) {
-		let root1 = (-b + Math.sqrt(d)) / (2 * a);
-		let root2 = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(root1, root2);
-	}
-	if (d === 0) {
-		let root0 = -b / (2 * a);
-		arr.push(root0);
-	}
-	return arr;
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else {
+    arr = [];
+  }
+  return arr;
 }
-
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let per = parseInt(percent) / 100 / 12; //процентная ставка
