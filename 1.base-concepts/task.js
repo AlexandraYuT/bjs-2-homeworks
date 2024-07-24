9d3444b7127be2d52f85b3a8f1ffaed5ce9ec627
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-24 16:44:31 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,11 +6,7 @@ function solveEquation(a, b, c) {
   if (d > 0) {
     arr[0] = (-b + Math.sqrt(d)) / (2 * a);
     arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-  } else if (d === 0) {
-    arr[0] = -b / (2 * a);
-  } else {
-    arr = [];
-  }
+  } 
   return arr;
 }
 
@@ -20,20 +16,16 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let am = parseInt(amount); //общая стоимость
 	let cm = parseInt(countMonths); // срок кредита
 
-	if (isNaN(per) || per < 0) {
+	if (isNaN(percent) || percent < 0) {
 		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-	} else if (isNaN(con) || con < 0) {
+	} else if (isNaN(contribution) || contribution < 0) {
 		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-	} else if (isNaN(am) || am < 0) {
+	} else if (isNaN(amount) || amount < 0) {
 		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
 	} else {
-		if (isNaN(cm) || cm < 0) {
+		if (isNaN(countMonths) || countMonths < 0) {
 			return `Параметр "срок ипотеки" содержит неправильное значение ${countMonths}`;
 		}
-		let s = am - con; //тело кредита
-		let pay = s * (per + per / (((1 + per) ** cm) - 1)); //ежемесячная оплата
-		let totalAmount = (pay * cm).toFixed(2);
-		console.log(totalAmount);
-		return +totalAmount;
+
 	}
 }
