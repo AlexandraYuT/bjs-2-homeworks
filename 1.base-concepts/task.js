86e91b08d24d52bc94b2e5fd0be6409c6584d68b
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-24 19:01:39 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,26 +2,29 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = b ** 2 - 4 * a * c;
+	let d = (b ** 2 - 4 * a * c);
 	if (d > 0) {
-		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-	} else if (d === 0) {
-		arr[0] = -b / (2 * a);
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+	}
+	if (d === 0) {
+		let root0 = -b / (2 * a);
 	}
 	return arr;
 }
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let percent = parseInt(percent) / 100 / 12;
-	let contribution = parseInt(contribution); 
-	let amount = parseInt(amount);
-	let countMonths = parseInt(countMonths);
-	
-	let s = amount - contribution;
-    	let pay = s * (percent + percent / (((1 + percent) ** cm) - 1));
-   	let totalAmount = (pay * cm).toFixed(2);
-    	console.log(totalAmount);
-    	return +totalAmount;
+	let per = parseInt(percent) / 100 / 12;
+	let con = parseInt(contribution);
+	let am = parseInt(amount);
+	if (isNaN(per) || per < 0) {
+		return `false`;
+	} else {
+		let s = am - con;
+		let n = countMonths;
+		let pay = s * (per + per / (((1 + per) ** n) - 1));
+		let totalAmount = (pay * n).toFixed(2);
+		console.log(totalAmount);
+		return +totalAmount;
+	}
 }
