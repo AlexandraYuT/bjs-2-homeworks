aa441dc4a15a5c570ec327cccd19c95e572c99f8
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-24 18:48:44 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,17 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 - 4 * a * c;
-  if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-  } else if (d === 0) {
-    arr[0] = -b / (2 * a);
-  } else {
-    arr = [];
-  }
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	} else if (d === 0) {
+		arr[0] = -b / (2 * a);
+	}
+	return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let percent = parseInt(percent) / 100 / 12;
