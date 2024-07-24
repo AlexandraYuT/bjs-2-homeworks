b79a93148295d05508da842731a0e768050ed68b
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-24 18:44:48 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,10 +2,14 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
+  let d = b ** 2 - 4 * a * c;
   if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else {
+    arr = [];
   }
   return arr;
 }
