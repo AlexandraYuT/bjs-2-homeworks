08d265a28c9bc7b0d47f20157d04d14584e1af90
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-07-24 18:17:50 +0400

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,7 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b ** 2 - 4 * a * c;
+  let discriminant = b ** 2 - 4 * a * c;
   if (d > 0) {
     arr[0] = (-b + Math.sqrt(d)) / (2 * a);
     arr[1] = (-b - Math.sqrt(d)) / (2 * a);
@@ -11,13 +11,13 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let per = parseInt(percent) / 100 / 12;
-	let con = parseInt(contribution); 
-	let am = parseInt(amount);
-	let cm = parseInt(countMonths);
+	let percent = parseInt(percent) / 100 / 12;
+	let contribution = parseInt(contribution); 
+	let amount = parseInt(amount);
+	let countMonths = parseInt(countMonths);
 	
-	let s = am - con;
-    	let pay = s * (per + per / (((1 + per) ** cm) - 1));
+	let s = amount - contribution;
+    	let pay = s * (percent + percent / (((1 + percent) ** cm) - 1));
    	let totalAmount = (pay * cm).toFixed(2);
     	console.log(totalAmount);
     	return +totalAmount;
